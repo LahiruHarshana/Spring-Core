1db7401684f09179ea5d7baaea2f0d7e0f579594
LahiruHarshana <lharshana2002@gmail.com>
2024-02-08 22:50:35 +0530

    updated

--- _01_Hello_Spring/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ _01_Hello_Spring/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -4,6 +4,6 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = ChuBean.class)
+@ComponentScan(basePackages = "lk.ijse.spring")
 public class AppConfig {
 }

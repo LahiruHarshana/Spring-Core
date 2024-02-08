629fcf74f767166c54a43d4339d1b89c2ab7cd4c
LahiruHarshana <lharshana2002@gmail.com>
2024-02-08 22:53:12 +0530

    updated

--- _01_Hello_Spring/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ _01_Hello_Spring/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PojoOne {
     public PojoOne() {
         System.out.println("PojoOne Instantiated");

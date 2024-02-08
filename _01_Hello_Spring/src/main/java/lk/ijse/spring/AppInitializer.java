0000f1f8c711502b5a0694dbc7bff4dc659c0df7
LahiruHarshana <lharshana2002@gmail.com>
2024-02-08 22:48:37 +0530

    updated

--- _01_Hello_Spring/src/main/java/lk/ijse/spring/AppInitializer.java
+++ _01_Hello_Spring/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.close();
     }
 }
  

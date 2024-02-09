492c736f8e5260d1f8ea710255514cd7bdea8541
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 16:44:11 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/Boy.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/Boy.java
@@ -1,11 +1,15 @@
 package lk.ijse.spring.bean;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component("Lahiru")
-public class Boy {
+public class Boy implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     @Autowired
     private GoodGirl goodGirl;
@@ -17,6 +21,31 @@ public class Boy {
 
     @PostConstruct
     public void initialize() {
-        System.out.println("");
+        System.out.println("Have I got a girl friend? " + goodGirl);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }

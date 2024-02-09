cf64815bd8d395668a530a28766e3b219811db14
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 16:34:45 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/Sachini.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/Sachini.java
@@ -4,8 +4,13 @@ package lk.ijse.spring.bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Sachini {
+public class Sachini implements GoodGirl{
     public Sachini() {
         System.out.println("sachini - Constructor");
+    }
+
+    @Override
+    public void love() {
+
     }
 }

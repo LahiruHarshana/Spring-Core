d89415698d087d17076cc345bd1c857c327d8df2
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 14:47:31 +0530

    updated

--- _02_SpringBeanLifeCycle/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ _02_SpringBeanLifeCycle/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -1,12 +1,40 @@
 package lk.ijse.spring.bean;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean() {
         System.out.println("SpringBean Instantiated");
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
 }

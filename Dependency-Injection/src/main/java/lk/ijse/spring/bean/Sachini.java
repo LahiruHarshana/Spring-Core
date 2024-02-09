3d8b614f2cbd5b622f7425856f7b5c0db1d3d065
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 16:44:58 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/Sachini.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/Sachini.java
@@ -1,16 +1,38 @@
 package lk.ijse.spring.bean;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Sachini implements GoodGirl{
+public class Sachini implements GoodGirl, BeanNameAware, BeanFactoryAware, InitializingBean, DisposableBean {
     public Sachini() {
         System.out.println("sachini - Constructor");
     }
 
     @Override
     public void love() {
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
 
     }
 }

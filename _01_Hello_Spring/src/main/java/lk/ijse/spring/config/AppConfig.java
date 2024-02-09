857d66b6c91aa11f42ede5908d0b6957b94b5a52
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 10:49:48 +0530

    updated

--- _01_Hello_Spring/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ _01_Hello_Spring/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"lk.ijse.spring","com.edu.ijse"})
+//@ComponentScan(basePackages = {"lk.ijse.spring","com.edu.ijse"})
+@ComponentScan(basePackageClasses = {lk.ijse.spring.pojo.PojoOne.class, com.edu.ijse.Bean.class})
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiated");

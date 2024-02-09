8116b39436a93c2f888ef70e71c0310490514ab0
LahiruHarshana <lharshana2002@gmail.com>
2024-02-09 16:37:39 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/Boy.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/Boy.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("Lahiru")
 public class Boy {
 
+    @Autowired
     private GoodGirl goodGirl;
     public Boy() {
         System.out.println("Boy - Constructor");

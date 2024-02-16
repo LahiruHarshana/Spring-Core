02da32cb0b8575d38f894a061e80321446b8b7b3
LahiruHarshana <lharshana2002@gmail.com>
2024-02-16 20:01:37 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -3,10 +3,15 @@ package lk.ijse.spring.bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class SpringBean {
-    SpringBean(String []name){
-        System.out.println(Arrays.toString(name));
+    SpringBean(Collection<String> mySet){
+        System.out.println("Spring Bean Instantiated");
+        System.out.println(mySet);
     }
 }
+

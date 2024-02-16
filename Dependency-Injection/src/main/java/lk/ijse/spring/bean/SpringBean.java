066c7ad8bed3db88d2bf4eddc2df39dc8161f426
LahiruHarshana <lharshana2002@gmail.com>
2024-02-16 15:43:57 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -2,9 +2,11 @@ package lk.ijse.spring.bean;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class SpringBean {
-    SpringBean(String name){
-        System.out.println(name);
+    SpringBean(String []name){
+        System.out.println(Arrays.toString(name);
     }
 }

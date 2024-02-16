4a787b586e4a087d61872949efac05800f8633c2
LahiruHarshana <lharshana2002@gmail.com>
2024-02-16 12:19:15 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/Tharushi.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/Tharushi.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Tharushi implements GoodGirl{
+    public Tharushi() {
+        System.out.println("Tharushi - Constructor");
+    }
     @Override
     public void love() {
 

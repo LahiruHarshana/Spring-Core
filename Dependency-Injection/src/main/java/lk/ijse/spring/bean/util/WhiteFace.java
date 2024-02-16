cb45798d2ed79db400397d5da14d2ecc3ee32a26
LahiruHarshana <lharshana2002@gmail.com>
2024-02-16 12:48:51 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/bean/util/WhiteFace.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/bean/util/WhiteFace.java
@@ -3,6 +3,12 @@ package lk.ijse.spring.bean.util;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 @Qualifier
 public @interface WhiteFace {
 

c546516cb1ad5174f51a9e819bb976c02d1da7c3
LahiruHarshana <lharshana2002@gmail.com>
2024-02-16 15:29:59 +0530

    updated

--- Dependency-Injection/src/main/java/lk/ijse/spring/AppConfig.java
+++ Dependency-Injection/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,10 +1,14 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
-
+    @Bean
+    public String myString(){
+        return "Hello Spring";
+    }
 }

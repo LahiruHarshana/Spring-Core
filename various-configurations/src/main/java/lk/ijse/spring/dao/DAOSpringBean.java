fc5aa752f8ace50aa77c83f39c14b765ad625c31
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 11:59:42 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/dao/DAOSpringBean.java
+++ various-configurations/src/main/java/lk/ijse/spring/dao/DAOSpringBean.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DAOSpringBean {
     public DAOSpringBean() {
         System.out.println("DAOSpringBean Instantiated via Spring");

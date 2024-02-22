52df0d692baabc8cc02c23ab20246553aa9d1ae5
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 09:55:11 +0530

    updated

--- _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean4.java
+++ _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean4.java
@@ -1,8 +1,38 @@
 package lk.ijse.spring.bean;
 
-/*springBean4 is in light mode*/
-public class SpringBean4 {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/*springBean4 is in full mode*/
+public class SpringBean4 implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean{
     public SpringBean4() {
         System.out.println("Spring Bean4 Instantiated...");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware.setBeanFactory()");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }

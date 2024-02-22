7c1b0578a2f8835dbaa592c1230aad90f8d3d984
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 12:48:56 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppConfig.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -7,5 +8,8 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan
 public class AppConfig {
-
+    @Bean
+    public String name(){
+        return "SpringBean1";
+    }
 }

3db1c96c5eedea6a40cd0bd72a952a4c8930c441
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 16:12:12 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean3.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean3.java
@@ -1,12 +1,22 @@
 package lk.ijse.spring.bean;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean3 {
+    @Autowired
     private Environment environment;
     public SpringBean3() {
         System.out.println("SpringBean3 Instantiated via Spring");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("SpringBean3 Initialized....");
+        String name = environment.getProperty("USER");
+        System.out.println(name);
     }
 }

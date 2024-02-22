54029d864d78f751fb2f6c03225ec97da42b82ab
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 15:27:51 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppInitializer.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -12,17 +12,17 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
-
-        Map<String, String> getenv = System.getenv();
-
-        for (Map.Entry<String, String> entry : getenv.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-
-        Properties properties = System.getProperties();
-
-        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+//
+//        Map<String, String> getenv = System.getenv();
+//
+//        for (Map.Entry<String, String> entry : getenv.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+//
+//        Properties properties = System.getProperties();
+//
+//        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
     }
 }

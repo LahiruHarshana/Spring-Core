d7eff406497f11f6e08e8229ead832de54f44576
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 09:38:10 +0530

    updated

--- _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.bean;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /*springBean1 is in full mode*/
@@ -10,4 +11,8 @@ public SpringBean1() {
         System.out.println("Spring Bean Instantiated...");
     }
 
+    @Bean
+    public SpringBean3 springBean3(){
+        return new SpringBean3();
+    }
 }

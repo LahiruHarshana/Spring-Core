5a9c174b53e3c7bf0013cfabc55a50ce3cf26bc9
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 13:59:13 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -9,10 +9,19 @@ public class SpringBean1 {
 
     @Value("Lahiru2")
     String name2;
-    public SpringBean1(@Value("Lahiru") String name,@Value("10") int age){
-        System.out.println("SpringBean1 Instantiated....");
-        System.out.println("Name: " + name);
-        System.out.println("Name2: " + name2);
+    public SpringBean1(@Value("Lahiru") String name,@Value("10") int age,@Value("10.0") double salary,@Value("true") boolean isEmployed,@Value("10") long contactNumber,@Value("10") short age2,@Value("10") byte age3,@Value("10") float salary2,@Value("{1,2,3,4,5,6,7,8,9}") int[] numbers){
+        System.out.println("SpringBean1 Instantiated via Spring");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(isEmployed);
+        System.out.println(contactNumber);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(salary2);
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
     @PostConstruct

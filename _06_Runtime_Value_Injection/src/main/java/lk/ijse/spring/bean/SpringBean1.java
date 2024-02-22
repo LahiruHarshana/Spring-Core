db7ad151c3da1bbf8768930f212d6d3fe83db1f5
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 13:00:19 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -1,10 +1,15 @@
 package lk.ijse.spring.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean1 {
-    public SpringBean1(String name) {
+
+    @Value("Lahiru")
+    String name2;
+    public SpringBean1(@Value("Lahiru") String name) {
         System.out.println("SpringBean1 Instantiated....");
+        System.out.println("Name: " + name);
     }
 }

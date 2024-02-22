453fd547c2fa2da3c58aec4521a3ba8cb0004f97
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 16:09:15 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean3.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean3.java
@@ -1,9 +1,11 @@
 package lk.ijse.spring.bean;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean3 {
+    private Environment environment;
     public SpringBean3() {
         System.out.println("SpringBean3 Instantiated via Spring");
     }

ea14400078c65d86a7b6f33350ea7be8b7b42ad7
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 13:59:42 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -9,7 +9,7 @@ public class SpringBean1 {
 
     @Value("Lahiru2")
     String name2;
-    public SpringBean1(@Value("Lahiru") String name,@Value("10") int age,@Value("10.0") double salary,@Value("true") boolean isEmployed,@Value("10") long contactNumber,@Value("10") short age2,@Value("10") byte age3,@Value("10") float salary2,@Value("{1,2,3,4,5,6,7,8,9}") int[] numbers){
+    public SpringBean1(@Value("Lahiru") String name,@Value("10") int age,@Value("10.0") double salary,@Value("true") boolean isEmployed,@Value("10") long contactNumber,@Value("10") short age2,@Value("10") byte age3,@Value("10") float salary2,@Value("1,2,3,4,5,6,7,8,9") int[] numbers){
         System.out.println("SpringBean1 Instantiated via Spring");
         System.out.println(name);
         System.out.println(age);

69657244f495e3ff0edb4bd31c523f749f70a544
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 16:15:00 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppConfig.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,12 +1,11 @@
 package lk.ijse.spring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class AppConfig {
     @Bean
     public String name(){

d4fa6e120b4f6877b7082652e697c59f8fa6936b
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 12:05:24 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/BOConfig.java
+++ various-configurations/src/main/java/lk/ijse/spring/BOConfig.java
@@ -1,5 +1,14 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.bo.BOSpringBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BOConfig {
 
+    @Bean
+    public BOSpringBean boSpringBean() {
+        return new BOSpringBean();
+    }
 }
--- various-configurations/src/main/java/lk/ijse/spring/bo/BOSpringBean.java
+++ various-configurations/src/main/java/lk/ijse/spring/bo/BOSpringBean.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BOSpringBean {
-    BOSpringBean() {
+    public BOSpringBean() {
         System.out.println("BOSpringBean Instantiated via Spring");
     }
 }

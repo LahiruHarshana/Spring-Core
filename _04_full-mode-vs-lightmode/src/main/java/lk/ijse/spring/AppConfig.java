2995c501757cca4a188cc4a1efd65c5faeb9f084
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 09:25:10 +0530

    updated

--- _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/AppConfig.java
+++ _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,8 +1,10 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
 
 }

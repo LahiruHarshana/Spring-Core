1c531486d334cea87f22d7281dc1e0d8f9d3a123
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 16:37:34 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppInitializer.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -1,4 +1,7 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 }

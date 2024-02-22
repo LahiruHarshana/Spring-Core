cdb1d691b6093e25dae6b6809a3e03900aec57a8
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 14:22:07 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -12,7 +14,7 @@ public class SpringBean1 {
 
     @Value("Lahiru2")
     String name2;
-    public SpringBean1(@Value("Lahiru") String name, @Value("10") int age, @Value("10.0") double salary, @Value("true") boolean isEmployed, @Value("10") long contactNumber, @Value("10") short age2, @Value("10") byte age3, @Value("10") float salary2, @Value("1,2,3,4,5,6,7,8,9") int[] numbers){
+    public SpringBean1(@Value("Lahiru") String name, @Value("10") int age, @Value("10.0") double salary, @Value("true") boolean isEmployed, @Value("10") long contactNumber, @Value("10") short age2, @Value("10") byte age3, @Value("10") float salary2, @Value("1,2,3,4,5,6,7,8,9") int[] numbers, @Value("1,2,3,4,5,6,7,8") List<String> list){
         System.out.println("SpringBean1 Instantiated via Spring");
         System.out.println(name);
         System.out.println(age);
@@ -27,7 +29,6 @@ public class SpringBean1 {
             System.out.println(number);
         }
     }
-
     @PostConstruct
     public void init(){
         System.out.println("SpringBean1 Initialized....");

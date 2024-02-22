e276ed00ef301b866771e56549a30cea41e07cb5
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 11:23:00 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/AppIntializer.java
+++ various-configurations/src/main/java/lk/ijse/spring/AppIntializer.java
@@ -1,4 +1,12 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppIntializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+    }
 }

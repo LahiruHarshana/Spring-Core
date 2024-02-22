36850c1fd0b52e45e2262ecb858dd84745e8a420
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 09:52:25 +0530

    updated

--- _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/AppConfig.java
+++ _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.bean.SpringBean2;
+import lk.ijse.spring.bean.SpringBean4;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +14,10 @@ public class AppConfig {
     @Bean
     public SpringBean2 springBean2(){
         return new SpringBean2();
+    }
+
+    @Bean
+    public SpringBean4 springBean4(){
+        return new SpringBean4();
     }
 }
--- _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean4.java
+++ _04_full-mode-vs-lightmode/src/main/java/lk/ijse/spring/bean/SpringBean4.java
@@ -1,7 +1,7 @@
 package lk.ijse.spring.bean;
 
 public class SpringBean4 {
-    SpringBean4() {
+    public SpringBean4() {
         System.out.println("Spring Bean4 Instantiated...");
     }
 }

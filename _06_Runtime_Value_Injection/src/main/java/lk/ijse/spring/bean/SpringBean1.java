431ba2f1a1d488d153ee9d7b6a98b5b19ac0d7fa
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 14:22:28 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -2,7 +2,6 @@ package lk.ijse.spring.bean;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import java.util.List;
 

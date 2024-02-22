e03937afe7b767ab1022eb0710ec454a2fa4acd2
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 16:43:16 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppInitializer.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -3,6 +3,10 @@ package lk.ijse.spring;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+    }
 }

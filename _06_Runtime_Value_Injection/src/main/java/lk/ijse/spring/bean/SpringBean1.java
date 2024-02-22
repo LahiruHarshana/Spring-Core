298ec56a29387c465533ac0fb2c8e21b02cf0d0a
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 12:40:45 +0530

    updated

---
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.bean;
+
+public class SpringBean1 {
+    private String name;
+
+    public SpringBean1() {
+        System.out.println("SpringBean1 Instantiated....");
+    }
+
+}

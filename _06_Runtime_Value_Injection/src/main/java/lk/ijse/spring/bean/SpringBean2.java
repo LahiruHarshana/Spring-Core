381b8035360596988d06b42d532201c56cf02b92
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 15:29:46 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean2.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean2.java
@@ -1,11 +1,12 @@
 package lk.ijse.spring.bean;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean2 {
-    public SpringBean2() {
+    public SpringBean2(@Value("${USERNAME}") String name) {
         System.out.println("SpringBean2 Instantiated via Spring");
     }
 }

61ccb7cd08475c73d6c8ed4ef5e392423d8437eb
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 11:59:34 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/bo/BOSpringBean.java
+++ various-configurations/src/main/java/lk/ijse/spring/bo/BOSpringBean.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.bo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BOSpringBean {
     BOSpringBean() {
         System.out.println("BOSpringBean Instantiated via Spring");

f02beb8451a6fc974cedb5413107167b0b01a67f
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 12:45:56 +0530

    updated

--- _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
+++ _06_Runtime_Value_Injection/src/main/java/lk/ijse/spring/bean/SpringBean1.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean1 {
-    private String name;
-
     public SpringBean1(String name) {
-
         System.out.println("SpringBean1 Instantiated....");
     }
 }

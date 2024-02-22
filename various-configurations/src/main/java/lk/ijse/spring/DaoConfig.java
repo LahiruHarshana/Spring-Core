232239f827c4dbaebf5b71e8eae52fd4722a04b6
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 12:05:43 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/DaoConfig.java
+++ various-configurations/src/main/java/lk/ijse/spring/DaoConfig.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.dao.DAOSpringBean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class DaoConfig {
+
+    public DAOSpringBean daoSpringBean() {
+        return new DAOSpringBean();
+    }
 }

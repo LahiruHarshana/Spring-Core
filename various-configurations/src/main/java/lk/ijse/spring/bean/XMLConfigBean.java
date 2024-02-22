20408a0c106325b95062c0597145c7fdd46a7149
LahiruHarshana <lharshana2002@gmail.com>
2024-02-22 11:24:36 +0530

    updated

--- various-configurations/src/main/java/lk/ijse/spring/bean/XMLConfigBean.java
+++ various-configurations/src/main/java/lk/ijse/spring/bean/XMLConfigBean.java
@@ -4,4 +4,7 @@ package lk.ijse.spring.bean;
 import org.springframework.context.annotation.ComponentScan;
 
 public class XMLConfigBean {
+    public XMLConfigBean() {
+        System.out.println("XML Config Bean Instantiated...");
+    }
 }

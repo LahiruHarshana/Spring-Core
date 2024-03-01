565a54a9fa7fcb9b574ef288db13153c091784b7
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 08:50:55 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppConfig.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/AppConfig.java
@@ -1,9 +1,19 @@
 package lk.ijse.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
+    public AppConfig() {
+        System.out.println("AppConfig Instantiated via Spring");
+    }
+
+    @Bean
+    public String name(){
+        return "SpringBean1";
+    }
+
 }

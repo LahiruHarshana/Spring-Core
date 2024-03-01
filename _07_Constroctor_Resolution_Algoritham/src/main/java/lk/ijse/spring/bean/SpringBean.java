84a8d33db2fc1a53ce68ffa732dc222ca53aa90c
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 08:55:17 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -13,5 +13,8 @@ public class SpringBean {
         this.age = age;
         this.isMale = isMale;
         System.out.println("SpringBean Instantiated via Spring");
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("isMale : " + isMale);
     }
 }

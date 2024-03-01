22f5483c90e068935d62d0ab1a603a456de7b8c4
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 08:57:27 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -8,11 +8,21 @@ public class SpringBean {
     int age;
     boolean isMale;
 
+    float height;
     public SpringBean(String name, int age, boolean isMale) {
         this.name = name;
         this.age = age;
         this.isMale = isMale;
         System.out.println("SpringBean Instantiated via Spring");
         System.out.printf("Name: %s, Age: %d, IsMale: %b\n", name, age, isMale);
+    }
+
+    public SpringBean(String name, int age, boolean isMale, float height) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        this.height = height;
+        System.out.println("SpringBean Instantiated via Spring");
+        System.out.printf("Name: %s, Age: %d, IsMale: %b, Height: %f\n", name, age, isMale, height);
     }
 }

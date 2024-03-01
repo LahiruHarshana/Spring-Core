f3c1cf5142bfaf1ea8f40edcde00a8aafbdb5f40
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 10:04:53 +0530

    updated

--- _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
+++ _07_Constroctor_Resolution_Algoritham/src/main/java/lk/ijse/spring/bean/SpringBean.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.bean;
 
 import jakarta.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,6 +20,8 @@ public class SpringBean {
 //        System.out.printf("Name: %s, Age: %d, IsMale: %b\n", name, age, isMale);
 //    }
 
+
+
     public SpringBean(String name, int age, boolean isMale,@Nullable float height) {
         this.name = name;
         this.age = age;
@@ -25,5 +29,33 @@ public class SpringBean {
         this.height = height;
         System.out.println("SpringBean Instantiated via Spring");
         System.out.printf("Name: %s, Age: %d, IsMale: %b, Height: %f\n", name, age, isMale, height);
+    }
+
+    @Autowired(required = false)
+    public SpringBean(String name, int age, boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        System.out.println("SpringBean Instantiated via Spring");
+        System.out.printf("Name: %s, Age: %d, IsMale: %b\n", name, age, isMale);
+    }
+
+
+    @Autowired(required = false)
+    public SpringBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("SpringBean Instantiated via Spring");
+        System.out.printf("Name: %s, Age: %d\n", name, age);
+    }
+
+    public SpringBean(String name) {
+        this.name = name;
+        System.out.println("SpringBean Instantiated via Spring");
+        System.out.printf("Name: %s\n", name);
+    }
+
+    public SpringBean() {
+        System.out.println("SpringBean Instantiated via Spring");
     }
 }
